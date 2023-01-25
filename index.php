1348e945aad374951a554252e44fe141d5653689
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-25 13:26:03 +0300

    created a homepage file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+session_reset();
+   $_SESSION;
+
+?>
+
+<html>
+    <head>
+        <title>My Website</title>
+    </head>
+    <body>
+        <a href="logout.php">logout</a>
+       <h1>This Is The Index page</h1>
+       <br>
+       Hello, username
+    </body>
+</html>
