042fb29b9ec3ff6df62c3d9a7f6e4aa244041959
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-26 10:23:49 +0300

    added authentication and sesions

--- signup.php
+++ signup.php
@@ -8,6 +8,15 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     // SOMETHING WAS POSTED
    $user_name = $_POST['user_name'];
    $password = $_POST['password'];
+
+   if(!empty($user_name) && !empty($password) && !is_numeric($user_name)){
+
+    // save to database
+    $user_id = $random_num(20);
+        $query = "insert into users (user_id,user_name,password) values ('$user_id', '$user_name','$password')";
+   } else {
+        echo "Please enter some valid information";
+   };
 }
      
 ?>
