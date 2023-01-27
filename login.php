5be7d7d8fe7b9af3f3e26a48cc4d677faf9d094d
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2023-01-27 16:20:28 +0300

    added an error message

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
                 $user_data = mysqli_fetch_assoc($results);
 
-                if ($user_data['password'] === $password);{
+                if ($user_data['password'] === $password){
                     $_SESSION['user_id'] = $user_data['user_id'];
                     header("Location: index.php");
                     die;
